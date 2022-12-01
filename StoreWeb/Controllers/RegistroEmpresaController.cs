f39ac921708f320e8f93a887c6e8ce9c518084c6
lidioguedez <112365426+lidioguedez@users.noreply.github.com>
2022-12-01 14:46:42 -0300

    Creabdo Controlador registro de empresas

---
+++ StoreWeb/Controllers/RegistroEmpresaController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoreWeb.Controllers
+{
+    public class RegistroEmpresaController : Controller
+    {
+        // GET: RegistroEmpresa
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: RegistroEmpresa/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RegistroEmpresa/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RegistroEmpresa/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RegistroEmpresa/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RegistroEmpresa/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RegistroEmpresa/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RegistroEmpresa/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

fc3d91fcaec7d128ec3d80da32e4c7e1e6ac9427
lidioguedez <112365426+lidioguedez@users.noreply.github.com>
2022-12-02 22:14:49 -0300

    Se crea Cliente y Clases para hacer consultas
    Clase 9

--- StoreWeb/Controllers/RegistroEmpresaController.cs
+++ StoreWeb/Controllers/RegistroEmpresaController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using StoreWeb.Entity.Parameters;
+using StoreWeb.Logic;
+using StoreWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +11,21 @@ namespace StoreWeb.Controllers
 {
     public class RegistroEmpresaController : Controller
     {
-        public ActionResult RegistroEmpresa()
+        private modelList model;
+        private BUPais bupais;
+        public RegistroEmpresaController()
         {
+            //Aqui se usa una mala practica de este profesor que da el curso 
+            model = new modelList();
+            bupais = new BUPais();
+        }
+
+        public ActionResult RegistroEmpresa(ENRegistroEmpresa paramss)
+        {
+            string token = "";
+
+            model.listPais = bupais.listarPaises(paramss, token);
+
             return View();
         }
 
